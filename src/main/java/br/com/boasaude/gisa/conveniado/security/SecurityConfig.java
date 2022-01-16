ef67cb47cfd3584c5e24c6994b987f1cdd5f2d36
Maicon Klein <maiconrochak@gmail.com>
2022-01-16 14:20:13 -0300

    Remoção security

--- src/main/java/br/com/boasaude/gisa/conveniado/security/SecurityConfig.java
+++ src/main/java/br/com/boasaude/gisa/conveniado/security/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${auth0.audience}")
