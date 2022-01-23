6a4827ee533ae383a223561e81e70deab05933bd
Maicon Klein <maiconrochak@gmail.com>
2022-01-22 23:49:33 -0300

    correção clientId clientSecret

--- src/main/java/br/com/boasaude/gisa/conveniado/service/ManagementAPIService.java
+++ src/main/java/br/com/boasaude/gisa/conveniado/service/ManagementAPIService.java
@@ -1,11 +1,14 @@
 package br.com.boasaude.gisa.conveniado.service;
 
+import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.mgmt.ManagementAPI;
 import com.auth0.client.mgmt.filter.RolesFilter;
 import com.auth0.client.mgmt.filter.UserFilter;
 import com.auth0.exception.Auth0Exception;
+import com.auth0.json.auth.TokenHolder;
 import com.auth0.json.mgmt.Role;
 import com.auth0.json.mgmt.RolesPage;
+import com.auth0.net.AuthRequest;
 import com.auth0.net.Request;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,10 +23,10 @@ public class ManagementAPIService {
     public static final String DOMAIN = "dev-0yczl7li.us.auth0.com";
 
     public void atualizarUserRole(String token, String email) throws Auth0Exception {
-//        AuthAPI authAPI = new AuthAPI(DOMAIN, "3UL6Igy5CgPdABd4X18M2lcDKkoB7vqR", "6T0gorIHT2_OuHJvcKAgGVFvKlJk5RqL6--C1UVKLBAODQNLcL1GNkdnbT_Hnqrt");
-//        AuthRequest authRequest = authAPI.requestToken("https://"+DOMAIN+"/api/v2/");
-//        TokenHolder holder = authRequest.execute();
-        ManagementAPI mgmt = new ManagementAPI(DOMAIN, token.replace("Bearer ", ""));
+        AuthAPI authAPI = new AuthAPI(DOMAIN, "kZLqQQ0tgxilbLMpmLykYwxLR3N90EIh", "mKs-vezECedfb1voawvqfRbZJ0LeBjC847dtZF71w_5o_EHQl4H7LjR4lDh7NHn8");
+        AuthRequest authRequest = authAPI.requestToken("https://"+DOMAIN+"/api/v2/");
+        TokenHolder holder = authRequest.execute();
+        ManagementAPI mgmt = new ManagementAPI(DOMAIN, holder.getAccessToken());
 
         RolesFilter rolesFilter = new RolesFilter();
         rolesFilter.withName("Conveniado");
