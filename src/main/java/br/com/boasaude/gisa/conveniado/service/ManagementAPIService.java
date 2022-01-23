54aa290ac1183eb5110b5c980399ff454968e541
Maicon Klein <maiconrochak@gmail.com>
2022-01-23 09:59:33 -0300

    correção criacao role

--- src/main/java/br/com/boasaude/gisa/conveniado/service/ManagementAPIService.java
+++ src/main/java/br/com/boasaude/gisa/conveniado/service/ManagementAPIService.java
@@ -1,6 +1,5 @@
 package br.com.boasaude.gisa.conveniado.service;
 
-import com.auth0.client.HttpOptions;
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.mgmt.ManagementAPI;
 import com.auth0.client.mgmt.filter.RolesFilter;
@@ -9,9 +8,11 @@ import com.auth0.exception.Auth0Exception;
 import com.auth0.json.auth.TokenHolder;
 import com.auth0.json.mgmt.Role;
 import com.auth0.json.mgmt.RolesPage;
+import com.auth0.json.mgmt.users.UsersPage;
 import com.auth0.net.AuthRequest;
 import com.auth0.net.Request;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,13 +20,14 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ManagementAPIService {
 
     public static final String DOMAIN = "dev-0yczl7li.us.auth0.com";
 
     public void atualizarUserRole(String token, String email) throws Auth0Exception {
         AuthAPI authAPI = new AuthAPI(DOMAIN, "3UL6Igy5CgPdABd4X18M2lcDKkoB7vqR", "6T0gorIHT2_OuHJvcKAgGVFvKlJk5RqL6--C1UVKLBAODQNLcL1GNkdnbT_Hnqrt");
-        AuthRequest authRequest = authAPI.requestToken("https://"+DOMAIN+"/api/v2/");
+        AuthRequest authRequest = authAPI.requestToken("https://" + DOMAIN + "/api/v2/");
         TokenHolder holder = authRequest.execute();
         ManagementAPI mgmt = new ManagementAPI(DOMAIN, holder.getAccessToken());
 
@@ -37,9 +39,16 @@ public class ManagementAPIService {
 
 
         UserFilter userFilter = new UserFilter();
-        mgmt.users().list(userFilter.withQuery("email=" + email)).execute();
+        UsersPage usersPage = mgmt.users().list(userFilter.withQuery("email=" + email)).execute();
 
-        mgmt.users().addRoles("1", roleIdList).execute();
+        usersPage.getItems().stream().findAny().ifPresent(user -> {
+            try {
+                mgmt.users().addRoles(user.getId(), roleIdList).execute();
+            } catch (Auth0Exception e) {
+                log.error("Erro durante criacao de role, {}",e.getMessage(), e);
+            }
+        });
+
 
     }
 }
