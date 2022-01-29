43bdbcb2e4eb47a76beba5b2ab4f455a7cf09c9f
Maicon Klein <maiconrochak@gmail.com>
2022-01-29 17:00:51 -0300

    Remoção de EnderecoRepository.java

--- src/main/java/br/com/boasaude/gisa/user/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.boasaude.gisa.user.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
-}
