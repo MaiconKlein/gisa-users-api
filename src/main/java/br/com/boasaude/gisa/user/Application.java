5f3ef5de9e49ec82ebe6c9d21d79b1592caa401e
Maicon Klein <maiconrochak@gmail.com>
2022-03-27 15:01:01 -0300

    open API

--- src/main/java/br/com/boasaude/gisa/user/Application.java
+++ src/main/java/br/com/boasaude/gisa/user/Application.java
@@ -1,15 +1,18 @@
 package br.com.boasaude.gisa.user;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@OpenAPIDefinition(info = @Info(title = "Usuários API", version = "1.0", description = "Cadastro de Usuários"))
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- src/main/java/br/com/boasaude/gisa/user/controller/UserController.java
+++ src/main/java/br/com/boasaude/gisa/user/controller/UserController.java
@@ -3,6 +3,11 @@ package br.com.boasaude.gisa.user.controller;
 import br.com.boasaude.gisa.user.dto.UserDto;
 import br.com.boasaude.gisa.user.service.UserService;
 import com.auth0.exception.Auth0Exception;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,28 +22,56 @@ import javax.validation.constraints.NotEmpty;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping
-    public ResponseEntity<String> listar() {
+    @Operation(summary = "Listar Usuários",
+            description = "Retorna uma lista de usuários")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna a lista de usuário"),
+            @ApiResponse(responseCode = "403",
+                    description = "Você não tem permissão para acessar este recurso",
+                    content = @Content(examples = @ExampleObject))})
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<UserDto> listar() {
         return new ResponseEntity(userService.listar(), HttpStatus.OK);
     }
 
-    @PostMapping
+    @Operation(summary = "Criar Usuário",
+            description = "Executa a criação de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Retorna o usuário criado"),
+            @ApiResponse(responseCode = "403",
+                    description = "Você não tem permissão para acessar este recurso",
+                    content = @Content(examples = @ExampleObject))})
+    @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<UserDto> criar(@Valid @RequestBody UserDto userDto,
                                          @NotEmpty @RequestHeader String role) throws Auth0Exception {
         UserDto dto = userService.criar(userDto, role);
         return new ResponseEntity(dto, HttpStatus.CREATED);
     }
 
-    @PutMapping
+    @Operation(summary = "Atualizar Usuário",
+            description = "Executa a atualização de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Retorna o usuário atualizado"),
+            @ApiResponse(responseCode = "403",
+                    description = "Você não tem permissão para acessar este recurso",
+                    content = @Content(examples = @ExampleObject))})
+    @PutMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<UserDto> atualizar(@Valid @RequestBody UserDto userDto,
                                              @NotEmpty @RequestHeader String role) throws Auth0Exception {
-        return new ResponseEntity(userService.atualizar(userDto, role), HttpStatus.CREATED);
+        return new ResponseEntity(userService.atualizar(userDto, role), HttpStatus.ACCEPTED);
     }
 
+    @Operation(summary = "Excluir Usuário",
+            description = "Executa a exclusão de um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "403",
+                    description = "Você não tem permissão para acessar este recurso",
+                    content = @Content(examples = @ExampleObject))})
     @DeleteMapping
     public ResponseEntity<Void> deletar(@Valid @RequestParam Long id) {
         userService.delete(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
 }
